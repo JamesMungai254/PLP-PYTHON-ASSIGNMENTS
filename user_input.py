463b4e97087d9cb7918ab6bbc41e94d2e44a9146
JamesMungai254 <mungaijames6303@gmail.com>
2024-10-30 12:21:23 +0300

    week 1 assignment

--- user_input.py
+++ user_input.py
@@ -4,4 +4,4 @@ age = input('How old are you? ')
 
 location = input('Where are you from? ')
 
-print(f'Hello {name}! You are {age} years old and you are from {location}.')
+print('Hello, ' + name + '! You are ' + age + ' years old and you are from ' + location + '.')
