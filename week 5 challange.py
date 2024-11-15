0d4ef8e39623d22f0f001e71d2bf45e435667ae8
JamesMungai254 <mungaijames6303@gmail.com>
2024-11-15 11:11:54 +0300

    week 5 challange

---
+++ week 5 challange.py
@@ -0,0 +1,27 @@
+class Car:
+    def move(self):
+        print("Driving 🚗")
+
+class Plane:
+    def move(self):
+        print("Flying ✈️")
+
+class Dog:
+    def move(self):
+        print("Running 🐕")
+
+class Fish:
+    def move(self):
+        print("Swimming 🐟")
+
+# Create instances of each class
+car = Car()
+plane = Plane()
+dog = Dog()
+fish = Fish()
+
+# Call the move method on each instance
+car.move()
+plane.move()
+dog.move()
+fish.move()
